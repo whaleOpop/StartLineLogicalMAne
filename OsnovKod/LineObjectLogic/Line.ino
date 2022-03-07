be85a02aa4661f5536323774b12c54d216b2db1e
whaleOpop <al-agabani-kit@yandex.ru>
2022-03-07 04:39:08 +0300

    main

--- OsnovKod/LineObjectLogic/Line.ino
+++ OsnovKod/LineObjectLogic/Line.ino
@@ -1,4 +1,3 @@
-
 #define L4 A0
 #define L3 A1
 #define L2 A2
@@ -8,34 +7,92 @@
 #define R3 A6
 #define R4 A7
 
-int linemax[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-int linemin[8];
-int linelast[8];
+class liners
+{
+private:
 
-class line {
-  private: int linemax[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-  
-    int linemin[8];
-    int linelast[8];
+  int linemax[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  int line[8];
+    int linelast[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int linemin[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+public:
+  void setLine(int *line_root)
+  {
+    line[8] = *line_root;
+  }
+  void getLine()
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      Serial.print(" ");
+      Serial.print(line[i]);
+    }
+    Serial.println();
+    Serial.println("Vivod");
+  }
+  void getMaxLine()
+  {
+
+    for (int i = 0; i < 8; i++)
+    {
+      Serial.print(" ");
+      Serial.print(linemax[i]);
+    }
+    Serial.println();
+    Serial.println("Max");
+  }
+  void getMinLine()
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      Serial.print(" ");
+      Serial.print(linemin[i]);
+    }
+    Serial.println();
+    Serial.println("Min");
+  }
+
+
+
+  void onMinLine()
+  {
+
+    for (int i = 0; i < 8; i++)
+    {
+      if (line[i] < linelast[i])
+      {
+        linemin[i] = line[i];
+      }
+    }
+    for (int i = 0; i < 8; i++)
+    {
+      linelast[i] = line[i];
+    }
+  }
+
+
+
+
+  void onMaxLine()
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      if (line[i] > linemax[i])
+      {
+        linemax[i] = line[i];
+      }
+    }
+  }
+
+
+
+  int error(){
     
-  public:
-  
-    void vivod() {
-
-    }
-    void getLine() {
-
-    }
-    void getMinLine() {
-
-    }
-    void getMaxLine() {
-
-    }
+  }
 };
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(L4, INPUT);
   pinMode(L3, INPUT);
@@ -47,55 +104,16 @@ void setup() {
   pinMode(R4, INPUT);
 }
 
-
-void loop() {
-
-  int line[8] = {map(analogRead(A0), 0, 1024, 0, 255)
-                 , map(analogRead(A1), 0, 1024, 0, 255),
-                 map(analogRead(A2), 0, 1024, 0, 255),
-                 map(analogRead(A3), 0, 1024, 0, 255),
-                 map(analogRead(A4), 0, 1024, 0, 255),
-                 map(analogRead(A5), 0, 1024, 0, 255),
-                 map(analogRead(A6), 0, 1024, 0, 255),
-                 map(analogRead(A7), 0, 1024, 0, 255)
-                };
-
-  for (int i = 0; i < 8; i++) {
-
-    Serial.print(" ");
-    Serial.print(line[i]);
-
-  }
-  Serial.println();
-  Serial.println("Вывод");
-
-
-  for (int i = 0; i < 8; i++) {
-    if (line[i] > linemax[i]) {
-      linemax[i] = line[i];
-    }
-    if (line[i] < linelast[i]) {
-      linemin[i] = line[i];
-
-    }
-  }
-  for (int i = 0; i < 8; i++) {
-    linelast[i] = line[i];
-  }
-
-  for (int i = 0; i < 8; i++) {
-    Serial.print(" ");
-    Serial.print(linemax[i]);
-
-  }
-  Serial.println();
-  Serial.println("макс");
-
-
-  for (int i = 0; i < 8; i++) {
-    Serial.print(" ");
-    Serial.print(linemin[i]);
-  }
-  Serial.println();
-  Serial.println("мин");
+void loop()
+{
+  int liner[8] = {map(analogRead(A0), 0, 1024, 0, 255),
+                  map(analogRead(A1), 0, 1024, 0, 255),
+                  map(analogRead(A2), 0, 1024, 0, 255),
+                  map(analogRead(A3), 0, 1024, 0, 255),
+                  map(analogRead(A4), 0, 1024, 0, 255),
+                  map(analogRead(A5), 0, 1024, 0, 255),
+                  map(analogRead(A6), 0, 1024, 0, 255),
+                  map(analogRead(A7), 0, 1024, 0, 255)};
+  liners line;
+  line.setLine(liner);
 }
