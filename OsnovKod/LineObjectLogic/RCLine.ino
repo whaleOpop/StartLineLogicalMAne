8a29bba697644310d9fe19cfa2ff3c2191fdd136
whaleOpop <al-agabani-kit@yandex.ru>
2022-04-18 14:02:38 +0300

    TestRC

--- OsnovKod/LineObjectLogic/RCLine.ino
+++ OsnovKod/LineObjectLogic/RCLine.ino
@@ -1,5 +1,4 @@
-#include "PID.h"
-#include "Line.h"
+
 // PIN L198n
 #define ena 2
 #define enb 3
@@ -7,6 +6,94 @@
 #define in2 41
 #define in3 42
 #define in4 43
+
+
+class Line
+{
+private:
+  int size;
+  int *line;
+
+public:
+  //[Возвращает ошибку робота]
+  int robotFlag()
+  {
+    int pos = 0;
+
+    if (line[0] >= 3 && line[0] < 600)
+      pos = 2;
+    if (line[1] >= 3 && line[1] < 600)
+      pos = 1;
+    if (line[2] >= 3 && line[2] < 600)
+      pos = 0;
+    if (line[3] >= 3 && line[3] < 600)
+      pos = -1;
+    if (line[4] >= 3 && line[4] < 600)
+      pos = -2;
+    Serial.println("flagPosition " + pos);
+    return pos;
+  }
+
+  //Выводит линию на которой находится робот
+  void getLine()
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      Serial.print(line[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  //Линия робота,размер массива линии
+  void setLine(int &line_data, int data_size)
+  {
+    size = data_size;
+    line = &line_data;
+  }
+};
+
+class PID
+{
+private:
+  int P;
+  int I;
+  int D;
+  int kp;
+  int kd;
+  int ki;
+  int PD;
+  int speed;
+  int error;
+  Line pos;
+  int err_p = -1;
+  int prevErr = pos.robotFlag();
+  ;
+  int err_arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+public:
+  //Коефицент Kp , Ki , Kd , ошибка робота pos.robotFlag()
+  void setLine(int kp_data, int ki_data, int kd_data, int errod_data)
+  {
+    kp = kp_data; // 10
+    kd = kd_data; // 5
+    ki = ki_data; // 5
+    error = errod_data;
+  }
+
+  int PIDr()
+  {
+    err_p = (err_p + 1) % 10;
+    err_arr[err_p] = error;
+    P = error * kp;
+    D = (err_arr[err_p] - err_arr[(err_p + 11) % 10]) * kd;
+    int err_sum = 0;
+    for (int i = 0; i < 10; i++)
+      err_sum += err_arr[i];
+    I = err_sum / 10 * ki;
+    prevErr = error;
+    return P + I + D;
+  }
+};
 
 void setup()
 {
@@ -100,8 +187,8 @@ void loop()
       digitalWrite(in2, HIGH);
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
-      analogWrite(ena, 255);
-      analogWrite(enb, 255);
+      analogWrite(ena, 225);
+      analogWrite(enb, 225);
     }
   }
 
@@ -116,8 +203,8 @@ void loop()
       digitalWrite(in2, LOW);
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
-      analogWrite(ena, 255);
-      analogWrite(enb, 255);
+      analogWrite(ena, 225);
+      analogWrite(enb, 225);
     }
   }
   else
@@ -126,7 +213,7 @@ void loop()
     if (millis() - time_counterstop > 50)
     {
       time_counterstop = millis();
-      motorSeT(125, 125, pidl.PIDr(), pidr.PIDr());
+      motorSeT(150, 150, pidl.PIDr(), pidr.PIDr());
       Serial.println("Sanya lox");
     }
   }
