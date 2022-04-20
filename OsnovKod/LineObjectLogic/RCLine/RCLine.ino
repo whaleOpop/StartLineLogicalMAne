3b16776d2cf7531f259d3b89905338989bd5802e
whaleOpop <al-agabani-kit@yandex.ru>
2022-04-20 14:08:51 +0300

    RC Algritm  ver 2

--- OsnovKod/LineObjectLogic/RCLine/RCLine.ino
+++ OsnovKod/LineObjectLogic/RCLine/RCLine.ino
@@ -29,15 +29,19 @@ public:
   int robotFlag()
   {
     int pos = 0;
-
+    // 10000
     if (line[0] >= 3 && line[0] < 600)
       pos = 2;
+    // 01000
     if (line[1] >= 3 && line[1] < 600)
       pos = 1;
+    // 00100
     if (line[2] >= 3 && line[2] < 600)
       pos = 0;
+    // 00010
     if (line[3] >= 3 && line[3] < 600)
       pos = -1;
+    // 00001
     if (line[4] >= 3 && line[4] < 600)
       pos = -2;
 
@@ -127,9 +131,7 @@ int maxline[8];
 Line_pos pos;
 PID pidr;
 PID pidl;
-//Левый мотор + Ф-ция пида для левого колеса
-
-//Правый мотор - Ф-ция пида для левого колеса
+//Установливай значение пид-регулятора на моторы
 void motorSeT(int rmotor, int lmotor, int pidl, int pidr)
 {
   int speedl = lmotor + pidl;
@@ -181,20 +183,33 @@ unsigned long time_counterleft;
 unsigned long time_counterright;
 void loop()
 {
-
-  int size;
-
+  // объедениям линии в массив
   int line[5] = {analogRead(A0), analogRead(A1), analogRead(A2), analogRead(A3), analogRead(A4)};
-
+  //передаём линию и размер
   pos.setLine(*line, 5);
+  //выводим ошибку
   pos.getPos();
-  //правое колесо пид
-  pidr.setLine(40, 0, 0, pos.robotFlag()); // 35
-  //левое  колесо пид
-  pidl.setLine(40, 0, 0, pos.robotFlag()); // 35
 
-  // Serial.print("PID ");
-  if ((line[4] >= 3 && line[4] < 600) && (line[3] >= 3 && line[3] < 600) && (line[2] >= 3 && line[2] < 600))
+  //правое колесо пид
+  pidr.setLine(35, 0, 0, pos.robotFlag()); // 35
+  //левое  колесо пид
+  pidl.setLine(35, 0, 0, pos.robotFlag()); // 35
+
+  //Уловие проезда волнистой и квадратной линии
+  // 11000
+  if ((line[0] >= 3 && line[0] < 600) && (line[1] >= 3 && line[1] < 600))
+  {
+    Serial.println("square right");
+
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    analogWrite(ena, 225);
+    analogWrite(enb, 225);
+  }
+  // 00011
+  else if ((line[4] >= 3 && line[4] < 600) && (line[3] >= 3 && line[3] < 600))
   {
     Serial.println("square left");
 
@@ -205,6 +220,20 @@ void loop()
     analogWrite(ena, 225);
     analogWrite(enb, 225);
   }
+  // 00111
+  else if ((line[4] >= 3 && line[4] < 600) && (line[3] >= 3 && line[3] < 600) && (line[2] >= 3 && line[2] < 600))
+  {
+    Serial.println("square left");
+
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(ena, 225);
+    analogWrite(enb, 225);
+  }
+  // 11100
+
   else if ((line[0] >= 3 && line[0] < 600) && (line[1] >= 3 && line[1] < 600) && (line[2] >= 3 && line[2] < 600))
   {
     Serial.println("square right");
@@ -213,21 +242,11 @@ void loop()
     digitalWrite(in2, LOW);
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    analogWrite(ena, 210);
-    analogWrite(enb, 210);
+    analogWrite(ena, 225);
+    analogWrite(enb, 225);
   }
-  else if ((line[0] >= 3 && line[0] < 600) && (line[1] >= 3 && line[1] < 600))
-  {
-    Serial.println("square right");
-
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    analogWrite(ena, 210);
-    analogWrite(enb, 210);
-  }
-  else if ((line[4] >= 3 && line[4] < 600) && (line[3] >= 3 && line[3] < 600))
+  // 01111
+  else if ((line[4] >= 3 && line[4] < 600) && (line[3] >= 3 && line[3] < 600) && (line[2] >= 3 && line[2] < 600) && (line[1] >= 3 && line[1] < 600))
   {
     Serial.println("square left");
 
@@ -235,16 +254,26 @@ void loop()
     digitalWrite(in2, HIGH);
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
-    analogWrite(ena, 210);
-    analogWrite(enb, 210);
+    analogWrite(ena, 225);
+    analogWrite(enb, 225);
   }
+  // 11110
+  else if ((line[0] >= 3 && line[0] < 600) && (line[1] >= 3 && line[1] < 600) && (line[2] >= 3 && line[2] < 600) && (line[3] >= 3 && line[3] < 600))
+  {
+    Serial.println("square right");
+
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    analogWrite(ena, 225);
+    analogWrite(enb, 225);
+  }
+
   else
   {
-    if (millis() - time_counterstop > 50)
-    {
-      time_counterstop = millis();
-      motorSeT(150, 150, pidl.PIDoras(), pidr.PIDoras());
-      Serial.println("Sanya lox");
-    }
+    //Включаем моторы и передаём значение пид-регулятора
+    motorSeT(120, 120, pidl.PIDoras(), pidr.PIDoras());
+    Serial.println("Sanya lox");
   }
 }
